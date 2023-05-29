df5919539e8bf98ff1b67ee1a014ffa19429f902
Sorrosal <sergiosorrosalgayan@gmail.com>
2023-05-29 21:50:19 +0200

    Agregar archivos de proyecto.

---
+++ CrudNet7MVC/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Agregar los modelos aquí
+        
+        public DbSet<Contacto> Contacto { get; set; }
+            
+        
+    }
+}
